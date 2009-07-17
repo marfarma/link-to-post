8a46024fb1452c2db517674138ea61e547e5c96f
Ajcrea <Ajcrea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-17 14:23:42 +0000

    git-svn-id: http://svn.wp-plugins.org/link-to-post/tags/0.4@136025 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ link-to-post.php
@@ -0,0 +1,125 @@
+<?php
+/*
+Plugin Name: Link to Post
+Plugin URI: http://www.ajcrea.com/plugins/wordpress/plugin-wordpress-lier-un-article-avec-link-to-post.html
+Author: Ajcrea
+Author URI: http://ajcrea.com
+Version: 0.4
+*/
+function adup_option($name,$value){
+	if(strlen($value)==0) $value = 'off';
+	if(get_option($name) == FALSE)
+		add_option($name,$value);
+	else
+		update_option($name,$value);
+}
+
+function pl_init(){
+	$locale = get_locale ();
+	if ( empty($locale) )
+		$locale = 'en_US';
+
+	$mofile = dirname (__FILE__)."/locale/$locale.mo";
+	load_textdomain ('link2post', $mofile);
+	pl_addbuttons();
+}
+
+function pl_addbuttons() {
+   // Don't bother doing this stuff if the current user lacks permissions
+   if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
+     return;
+ 
+   // Add only in Rich Editor mode
+   if ( get_user_option('rich_editing') == 'true') {
+		add_filter("mce_external_plugins", "add_pl_tinymce_plugin");
+		add_filter('mce_buttons', 'register_pl_button');
+   }
+}
+ 
+function register_pl_button($buttons) {
+   array_push($buttons, "separator", "post_link","page_link");
+   return $buttons;
+}
+ 
+function add_pl_tinymce_plugin($plugin_array) {
+   $plugin_array['link2post'] = get_bloginfo('wpurl').'/wp-content/plugins/link-to-post/tinymce/editor_plugin.js';
+   return $plugin_array;
+}
+
+function pl_adminjavascript(){
+	?>
+	<script type="text/javascript" src="<?php bloginfo('wpurl'); ?>/wp-content/plugins/link-to-post/link2post.js"></script>
+	<script type="text/javascript">
+	//<![CDATA[
+	function edlinktopost() {
+		var content = getContentSelection(window);
+	   tb_show("<?php _e('Link a post','link2post'); ?>","<?php bloginfo('wpurl'); ?>/wp-content/plugins/link-to-post/posts.php?tri="+content+"&amp;validate=1&amp;where=both&amp;category=-1&amp;TB_iframe=true",false);
+	}
+	function edlinktopage() {
+		var content = getContentSelection(window);
+	   tb_show("<?php _e('Link a page','link2post'); ?>","<?php bloginfo('wpurl'); ?>/wp-content/plugins/link-to-post/pages.php?tri="+content+"&amp;validate=1&amp;where=both&amp;TB_iframe=true",false);
+	}
+	//]]>
+	</script>	
+	<?php
+}
+
+function pl_quicktags(){
+	$buttonshtml = '<input type="button" class="ed_button" onclick="edlinktopost(); return false;" title="' . __('Link a post','link2post') . '" value="' . __('Link a post','link2post') . '" />';
+	$buttonshtml .= '<input type="button" class="ed_button" onclick="edlinktopage(); return false;" title="' . __('Link a page','link2post') . '" value="' . __('Link a page','link2post') . '" />';
+	?>
+	<script type="text/javascript" charset="utf-8">
+	// <![CDATA[
+	   (function(){
+		  if (typeof jQuery === 'undefined') {
+			 return;
+		  }
+		  jQuery(document).ready(function(){
+			 jQuery("#ed_toolbar").append('<?php echo $buttonshtml; ?>');
+		  });
+	   }());
+	// ]]>
+	</script>
+	<?php
+}
+
+function pl_adminpage() {	
+	add_options_page('Link to post', 'Link to post', 8, __FILE__, 'pl_optionpage');	
+}
+
+function pl_optionpage(){
+	if(isset($_POST['Submit'])){
+		adup_option('pl_select',$_POST['select']);
+		adup_option('pl_nofollow',$_POST['nofollow']);
+	}
+	$select = get_option('pl_select');
+	$nofollow = get_option('pl_nofollow');
+	?>
+	<div class="wrap">
+		<h2><?php echo _e('Options of "Link to post"','link2post'); ?></h2>
+		
+		<h3><?php _e('Configuration','link2post'); ?></h3>
+			<form action="" method="post">
+				<p>
+					<input type="checkbox" name="select" id="select" <?php if($select == 'on') echo 'checked="checked"'; ?>/>
+					<label for="select"><?php _e('Search with selected text','link2post'); ?></label>
+				</p>
+				<p>
+					<input type="checkbox" name="nofollow" id="nofollow" <?php if($nofollow == 'on') echo 'checked="checked"'; ?>/>
+					<label for="nofollow"><?php _e('Add the nofollow attribute','link2post'); ?></label>
+				</p>
+				<p class="submit">
+					<input name="Submit" type="submit" class="button-primary" value="<?php _e('Save changes','link2post'); ?>" />
+				</p>
+			</form>
+	</div>
+	<?php
+}
+ 
+add_action('init', 'pl_init');
+add_action('admin_menu', 'pl_adminpage');
+add_action('admin_print_scripts','pl_adminjavascript');
+add_action( 'edit_form_advanced', 'pl_quicktags');
+add_action( 'edit_page_form', 'pl_quicktags');
+
+?>
---
+++ pages.php
@@ -0,0 +1,176 @@
+<?php
+require_once( dirname(dirname(dirname(dirname(__FILE__)))) . '/wp-config.php');
+global $wpdb;
+$nofollow = get_option('pl_nofollow');
+$bFirstAndSelect = 0;
+if(get_option('pl_select') == 'on' && $_REQUEST['validate'] == 1 && strlen($_REQUEST['tri'])>0)
+	$bFirstAndSelect = 1;
+?><html>
+<head>
+<script type='text/javascript' src='<?php bloginfo('wpurl'); ?>/wp-includes/js/jquery/jquery.js'></script>
+<script type='text/javascript' src='<?php bloginfo('wpurl'); ?>/wp-includes/js/quicktags.js'></script>
+<script type='text/javascript' src='<?php bloginfo('wpurl'); ?>/wp-includes/js/thickbox/thickbox.js'></script>
+<script type="text/javascript" src="<?php bloginfo('wpurl'); ?>/wp-content/plugins/link-to-post/link2post.js"></script>
+<link rel="stylesheet" href="<?php bloginfo('wpurl'); ?>/wp-admin/css/global.css" type="text/css">
+<link rel="stylesheet" href="<?php bloginfo('wpurl'); ?>/wp-admin/wp-admin.css" type="text/css">
+<link rel="stylesheet" href="<?php bloginfo('wpurl'); ?>/wp-admin/css/colors-fresh.css" type="text/css">
+<link rel='stylesheet' href='<?php bloginfo('wpurl'); ?>/wp-content/plugins/link-to-post/tinymce/css/link2post.css' type='text/css' />
+</head>
+<body>
+<div class="wrapper_quicktag">
+	<p id="showFilter"><a href="javascript:showFilter()"><?php _e('Show filter','link2post'); ?></a></p>
+	<fieldset id="filter">
+		<legend><?php _e('Filter pages','link2post'); ?></legend>
+		<form action="" method="GET" id="fc">
+			<p>
+				<label for="tri"><?php _e('Content','link2post'); ?></label>
+				<input type="text" name="tri" id="tri" value="<?php if(strlen($_REQUEST['validate'])>1 || $bFirstAndSelect) echo $_REQUEST['tri']; ?>"/>
+				<select name="where" id="where">
+					<option value="title" <?php if($_REQUEST['where'] == 'title') echo 'selected="selected"'; ?>><?php _e('in title','link2post'); ?></option>
+					<option value="content" <?php if($_REQUEST['where'] == 'content') echo 'selected="selected"'; ?>><?php _e('in content','link2post'); ?></option>
+					<option value="both" <?php if($_REQUEST['where'] == 'both' || strlen($_REQUEST['where']) == 0) echo 'selected="selected"'; ?>><?php _e('in both','link2post'); ?></option>
+				</select>
+			</p>
+			<p id="validate">
+				<input type="submit" class="mceButton" name="validate" id="validate" value="<?php _e('search','link2post'); ?>" />
+				<?php if(strlen($_REQUEST['tri'])>0 && (strlen($_REQUEST['validate'])>1 || $bFirstAndSelect)){ ?><a href="pages.php"><?php _e('Cancel','link2post'); ?></a><?php } ?>
+				<a href="javascript:hideFilter()"><?php _e('Hide filter','link2post'); ?></a>
+			</p>
+		</form>
+	</fieldset>
+	<?php
+
+	function pages($nb,$nbpages,$page,$where = 'both',$tri = ''){
+		global $bFirstAndSelect;
+		if(strlen($_REQUEST['validate'])>1 || $bFirstAndSelect)
+			$tri = $tri;
+		else $tri = '';
+		if(strlen($where)==0)
+			$where = 'both';
+		if(strlen($category)==0)
+			$category = -1;
+		echo '<p>';
+		echo '<span class="results">';
+		if($nb==1){
+			 echo '1 '.__('page','link2post');
+		}
+		elseif($nb>1){
+			echo $nb.' '.__('pages','link2post');
+		}
+		echo '</span>';
+		if($nbpages > 1){
+			for($i = 1;$i<=$nbpages;$i++){
+				if($nbpages>=8){
+					if($page > 4){
+						if($i == 1){
+							echo '<a href="pages.php?validate=validate&where='.$where.'&tri='.$tri.'&page='.$i.'">&lt;&lt;</a>&nbsp;&nbsp;';
+							continue;
+						}
+						else if($i < $page -3){ continue;}
+					}
+					if($page < $nbpages - 3){
+						if($i == $nbpages){
+							echo '<a href="pages.php?validate=validate&where='.$where.'&tri='.$tri.'&page='.$i.'">&gt;&gt;</a>';
+							continue;									
+						}
+						else if($i > $page +3){ continue; }
+					}
+				}
+				if($i == $page){ $bold1 = '<strong>'; $bold2 = '</strong>'; }
+				else { $bold1 = $bold2 = ''; }
+				echo '<a href="pages.php?validate=validate&where='.$where.'&tri='.$tri.'&page='.$i.'">'.$bold1.$i.$bold2.'</a>';
+				if($i != $nbpages) echo '&nbsp;&nbsp;';
+			}
+		}
+		echo '</p>';
+	}
+
+	function pl_trim_excerpt($text) {
+		if (strlen($text) > 0 ) {
+	 
+			$text = strip_shortcodes( $text );
+			$text = apply_filters('the_content', $text);
+			$text = str_replace(']]>', ']]&gt;', $text);
+			$text = strip_tags($text);
+			$excerpt_length = apply_filters('excerpt_length', 55);
+			$words = explode(' ', $text, $excerpt_length + 1);
+			if (count($words) > $excerpt_length) {
+				array_pop($words);
+				array_push($words, '[...]');
+				$text = implode(' ', $words);
+			}
+		}
+		return $text;
+	}
+		$where = '';
+		$type = ' ( post_type = "POST" OR post_type = "PAGE" )';
+		if(strlen($_REQUEST['validate'])>1 || $bFirstAndSelect){
+			if(strlen($_REQUEST['tri'])>0){
+				$mots = explode(' ',trim($_REQUEST['tri']));
+				switch($_REQUEST['where']){
+							case 'title':
+								if(count($mots)>1){
+									$where = ' AND ';
+									foreach($mots as $key=>$mot){
+										if($key == 0) $where .= ' ( ';
+										else $where .= ' AND ';
+										$where .= ' post_title LIKE "%'.htmlentities($mot).'%" ';
+										if($key == count($mots) - 1) $where .= ' ) ';
+									}
+								}
+								else
+									$where = ' AND post_title LIKE "%'.htmlentities($_REQUEST['tri']).'%" ';
+								
+							break;
+							case 'content':
+								if(count($mots)>1){
+									$where = ' AND ';
+									foreach($mots as $key=>$mot){
+										if($key == 0) $where .= ' ( ';
+										else $where .= ' AND ';
+										$where .= ' post_content LIKE "%'.htmlentities($mot).'%" ';
+										if($key == count($mots) - 1) $where .= ' ) ';
+									}
+								}
+								else
+								$where = ' AND post_content LIKE "%'.htmlentities($_REQUEST['tri']).'%" ';
+							break;
+							case 'both':
+								if(count($mots)>1){
+									$where = ' AND ';
+									foreach($mots as $key=>$mot){
+										if($key == 0) $where .= ' ( ';
+										else $where .= ' AND ';
+										$where .= ' ( post_title LIKE "%'.htmlentities($mot).'%" OR post_content LIKE "%'.htmlentities($mot).'%" ) ';
+										if($key == count($mots) - 1) $where .= ' ) ';
+									}
+								}
+								else
+								$where = ' AND ( post_title LIKE "%'.htmlentities($_REQUEST['tri']).'%" OR post_content LIKE "%'.htmlentities($_REQUEST['tri']).'%" ) ';
+							break;
+				}
+			}
+		}
+		$result = $wpdb->get_results('SELECT COUNT( * ) AS num_posts FROM '.$wpdb->posts.' WHERE  post_type = "PAGE" '.$where.' AND post_status = "publish" ');
+		$nb = $result[0]->num_posts;
+		$number = 15;
+		if(!isset($_GET['page'])){ $page = 1; }
+		$offset = $number * ($page-1);
+		$nbpages = ceil($nb/$number);
+		$posts = $wpdb->get_results('SELECT * FROM '.$wpdb->posts.' WHERE post_type="PAGE" '.$where.' AND post_status = "publish" ORDER BY post_date desc LIMIT '.$offset.','.$number.'');
+		if(count($posts)>0){
+			pages($nb,$nbpages,$page,$_REQUEST['where'],$_REQUEST['tri']);
+			echo '<ul id="liens">';
+			foreach($posts as $post){
+				$GLOBALS['post'] = $post;
+				echo '<li><a href="'.get_permalink($post->ID).'" onclick="return insertPostLink(this,\''.$nofollow.'\')" title="'.pl_trim_excerpt($post->post_content).'">'.get_the_title($post->ID).'</a></li>';
+			}
+			echo '</ul>';
+		}
+		else{
+			?><p><span class="results"><?php _e('No page','link2post'); ?></span></p><?php
+		}
+	?>
+	</div>
+</body>
+</html>
