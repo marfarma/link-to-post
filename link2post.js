5bb050b65c5e81b95d621a173a63a84887c96512
julienappert <julienappert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-25 12:01:55 +0000

    git-svn-id: http://svn.wp-plugins.org/link-to-post/tags/0.5@176920 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ link2post.js
@@ -0,0 +1,55 @@
+function getContentSelection(win){
+	var word = '', sel, startPos, endPos;
+	if (document.selection) {
+		win.edCanvas.focus();
+	    sel = document.selection.createRange();
+		if (sel.text.length > 0) {
+			word = sel.text;
+		}
+	}
+	else if (win.edCanvas.selectionStart || win.edCanvas.selectionStart == '0') {
+		startPos = win.edCanvas.selectionStart;
+		endPos = win.edCanvas.selectionEnd;
+		if (startPos != endPos) {
+			word = win.edCanvas.value.substring(startPos, endPos);
+		}
+	}
+	return word;
+}
+
+function insertPostLink(elem,nofollow,shortcode){
+	elem = jQuery(elem);
+	var winder = window.top;	
+	var href,title,rel = '',text;
+	var word = getContentSelection(winder);
+	if(word.length == 0){
+		var text = elem.text();
+	}
+	else{
+		var text = word;
+	}
+	if(shortcode == 'on'){
+		var id = elem.attr('id');
+		var link = '[link2post id="' + id + '"]' + text + '[/link2post]';
+	}
+	else{
+		var href = elem.attr('href');
+		var title = elem.text();
+		if(nofollow == 'on'){
+			var rel = 'rel="nofollow"';
+		}	
+		var link = '<a href="'+href+'" title="'+title+'" '+rel+'>'+text+'</a>';	
+	}
+
+    winder.edInsertContent(winder.edCanvas, link);
+	winder.tb_remove();
+	return false;
+}
+function showFilter(){
+	jQuery('#showFilter').css('display','none');
+	jQuery('#filter').css('display','block');
+}
+function hideFilter(){
+	jQuery('#showFilter').css('display','block');
+	jQuery('#filter').css('display','none');
+}
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Link to Post ===
+Contributors: julienappert,Ajcrea
+Tags: page, post, tinymce,link
+Requires at least: 2.6
+Tested up to: 2.8.6
+Stable tag: trunk
+
+This plugin allows you to easily create a link to an existing article or page in another article or page
+
+== Description ==
+
+This plugin allows you to easily create a link to an existing article or page in another article or page. It adds two buttons to the tinymce and the quicktags editors, which list the existing articles and pages, with the possibility of filtering by content or category.
+
+== Installation ==
+
+1. Download 'link-to-post.zip'
+2. unzip and upload 'link-to-post' directory to your '/wp-content/plugins' directory
+3. Go to the plugin management page and enable the plugin
+4. That's it !
+
+== Screenshots ==
+
+1. link to post backend
+2. link to post popup
+
+== Changelog ==
+
+= 0.5 =
+* links use html or shortcode
+* delete of excerpts in the popup in order to fix some bugs
+
