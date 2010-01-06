b42c9439edac43772ada4886918309b146d0fdd8
julienappert <julienappert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-06 09:33:15 +0000

    0.5.1
    git-svn-id: http://svn.wp-plugins.org/link-to-post/tags/0.5.1@190528 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ link-to-post.php
@@ -0,0 +1,154 @@
+<?php
+/*
+Plugin Name: Link to Post
+Plugin URI: http://julienappert.com/developpements-wordpress/plugin-link-to-post
+Author: Julien Appert
+Author URI: http://julienappert.com
+Version: 0.5.1
+Description: This plugin allows you to easily create a link to an existing article or page in another article or page.
+*/
+
+class WPLinkToPost{
+
+	function WPLinkToPost(){$this->__construct();}
+		
+	function __construct(){
+		add_action('init', array(&$this,'init'));
+		add_action('admin_menu', array(&$this,'adminpage'));
+		add_action('admin_print_scripts',array(&$this,'adminjavascript'));
+		add_action( 'edit_form_advanced', array(&$this,'quicktags'));
+		add_action( 'edit_page_form', array(&$this,'quicktags'));	
+		add_shortcode('link2post', array($this,'shortcode_func'));
+	}
+
+function shortcode_func($atts, $content = null) {
+	$permalink = get_permalink($atts['id']);
+	$title = get_the_title($atts['id']);
+	$nofollow = get_option('pl_nofollow');
+	if($nofollow == 'on') $rel = ' rel="nofollow"';
+	else $rel = '';
+	return '<a href="'.$permalink.'" title="'.$title.'"'.$rel.'>'.$content.'</a>';
+}
+	
+	
+	function option($name,$value){
+		if(strlen($value)==0) $value = 'off';
+		if(get_option($name) == FALSE)
+			add_option($name,$value);
+		else
+			update_option($name,$value);
+	}
+
+	function init(){
+		$locale = get_locale ();
+		if ( empty($locale) )
+			$locale = 'en_US';
+
+		$mofile = dirname (__FILE__)."/locale/$locale.mo";
+		load_textdomain ('link2post', $mofile);
+		$this->addbuttons();
+	}
+
+	function addbuttons() {
+	   // Don't bother doing this stuff if the current user lacks permissions
+	   if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
+		 return;
+	 
+	   // Add only in Rich Editor mode
+	   if ( get_user_option('rich_editing') == 'true') {
+			add_filter("mce_external_plugins", array(&$this,"add_tinymce_plugin"));
+			add_filter('mce_buttons', array(&$this,'register_button'));
+	   }
+	}
+ 
+	function register_button($buttons) {
+	   array_push($buttons, "separator", "post_link","page_link");
+	   return $buttons;
+	}
+ 
+	function add_tinymce_plugin($plugin_array) {
+	   $plugin_array['link2post'] = get_bloginfo('wpurl').'/wp-content/plugins/link-to-post/tinymce/editor_plugin.js';
+	   return $plugin_array;
+	}
+
+	function adminjavascript(){
+		?>
+		<script type="text/javascript" src="<?php bloginfo('wpurl'); ?>/wp-content/plugins/link-to-post/link2post.js"></script>
+		<script type="text/javascript">
+		//<![CDATA[
+		function edlinktopost() {
+			var content = getContentSelection(window);
+		   tb_show("<?php _e('Link a post','link2post'); ?>","<?php bloginfo('wpurl'); ?>/wp-content/plugins/link-to-post/posts.php?tri="+content+"&amp;validate=1&amp;where=both&amp;category=-1&amp;TB_iframe=true",false);
+		}
+		function edlinktopage() {
+			var content = getContentSelection(window);
+		   tb_show("<?php _e('Link a page','link2post'); ?>","<?php bloginfo('wpurl'); ?>/wp-content/plugins/link-to-post/pages.php?tri="+content+"&amp;validate=1&amp;where=both&amp;TB_iframe=true",false);
+		}
+		//]]>
+		</script>	
+		<?php
+	}
+
+	function quicktags(){
+		$buttonshtml = '<input type="button" class="ed_button" onclick="edlinktopost(); return false;" title="' . __('Link a post','link2post') . '" value="' . __('Link a post','link2post') . '" />';
+		$buttonshtml .= '<input type="button" class="ed_button" onclick="edlinktopage(); return false;" title="' . __('Link a page','link2post') . '" value="' . __('Link a page','link2post') . '" />';
+		?>
+		<script type="text/javascript" charset="utf-8">
+		// <![CDATA[
+		   (function(){
+			  if (typeof jQuery === 'undefined') {
+				 return;
+			  }
+			  jQuery(document).ready(function(){
+				 jQuery("#ed_toolbar").append('<?php echo $buttonshtml; ?>');
+			  });
+		   }());
+		// ]]>
+		</script>
+		<?php
+	}
+
+	function adminpage() {	
+		add_options_page('Link to post', 'Link to post', 8, __FILE__, array(&$this,'optionpage'));	
+	}
+
+	function optionpage(){
+		if(isset($_POST['Submit'])){
+			$this->option('pl_select',$_POST['select']);
+			$this->option('pl_nofollow',$_POST['nofollow']);
+			$this->option('pl_shortcode',$_POST['shortcode']);
+		}
+		$select = get_option('pl_select');
+		$nofollow = get_option('pl_nofollow');
+		$shortcode = get_option('pl_shortcode');
+		?>
+		<div class="wrap">
+			<h2><?php echo _e('Options of "Link to post"','link2post'); ?></h2>
+			
+			<h3><?php _e('Configuration','link2post'); ?></h3>
+				<form action="" method="post">
+					<p>
+						<input type="checkbox" name="select" id="select" <?php if($select == 'on') echo 'checked="checked"'; ?>/>
+						<label for="select"><?php _e('Search with selected text','link2post'); ?></label>
+					</p>
+					<p>
+						<input type="checkbox" name="nofollow" id="nofollow" <?php if($nofollow == 'on') echo 'checked="checked"'; ?>/>
+						<label for="nofollow"><?php _e('Add the nofollow attribute','link2post'); ?></label>
+					</p>
+					<p>
+						<input type="checkbox" name="shortcode" id="shortcode" <?php if($shortcode == 'on') echo 'checked="checked"'; ?>/>
+						<label for="shortcode"><?php _e('Use the shortcode','link2post'); ?></label> <strong><?php _e("(use with caution : if you disable the plugin, shortcode won't generate links anymore)","link2post"); ?></strong>
+					</p>					
+					<p class="submit">
+						<input name="Submit" type="submit" class="button-primary" value="<?php _e('Save changes','link2post'); ?>" />
+					</p>
+				</form>
+		</div>
+		<?php
+	}
+ 
+}
+
+new WPLinkToPost();
+
+?>
